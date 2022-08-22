8686ec26b3c1446b06cf384f76cc7e825efd0799
Caleb Elliott <caleb123elliott@gmail.com>
2022-08-22 14:32:43 -0600

    build

---
+++ docs/assets/AboutPage.f7ce19cf.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./index.6aa91402.js";import{o,b as t,e as s}from"./vendor.d67779e4.js";const a={name:"AboutPage"},n={class:"about"},r=s("h1",null,"This is the about page",-1),c=[r];function _(d,i,p,u,f,h){return o(),t("div",n,c)}var b=e(a,[["render",_]]);export{b as default};
